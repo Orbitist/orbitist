fc14d61a53509ccc1e87ae2e123da174adad1510
Nicholas Gunner <gunnernr@gmail.com>
2016-12-03 10:36:38 -0500

    validate that contents exist on submission

--- client/templates/stories/story_submit.js
+++ client/templates/stories/story_submit.js
@@ -1,3 +1,16 @@
+Template.storySubmit.onCreated(function() {
+  Session.set('storySubmitErrors', {});
+});
+
+Template.storySubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('storySubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('storySubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.storySubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -6,6 +19,10 @@ Template.storySubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
+
+    var errors = validateStory(story);
+    if (errors.title || errors.url)
+      return Session.set('storySubmitErrors', errors);
 
     Meteor.call('storyInsert', story, function(error, result) {
       // display the error to the user and abort
