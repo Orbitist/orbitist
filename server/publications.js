730a91e2fc6fee74f9ae435f8b12852d4c115d2b
Nicholas Gunner <gunnernr@gmail.com>
2016-12-06 08:30:28 -0500

    Small typo

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ Meteor.publish('stories', function() {
   return Stories.find();
 });
 
-Meteor.publish('tiles', function() {
+Meteor.publish('tiles', function(storyId) {
   check(storyId, String);
   return Tiles.find({storyId: storyId});
 });
