647e55f10a148a96930e7c77feb53825567ab3eb
Nicholas Gunner <gunnernr@gmail.com>
2016-12-07 08:36:14 -0500

    set a singleStory publication

--- server/publications.js
+++ server/publications.js
@@ -6,6 +6,11 @@ Meteor.publish('stories', function(options) {
   return Stories.find({}, options);
 });
 
+Meteor.publish('singleStory', function(id) {
+  check(id, String)
+  return Stories.find(id);
+});
+
 Meteor.publish('tiles', function(storyId) {
   check(storyId, String);
   return Tiles.find({storyId: storyId});
