9fe8482f9f1adb071a0ec362cd52dabdf91aa5ef
Nicholas Gunner <gunnernr@gmail.com>
2016-12-09 12:04:45 -0500

    tweaking code on the login screen

--- client/templates/includes/logout.js
+++ client/templates/includes/logout.js
@@ -2,6 +2,7 @@ if(Meteor.isClient){
   Template.logout.events({
   	'click .logout': function(event){
   		event.preventDefault();
+      Session.set('currentLoginScreen', 'login');
   		Meteor.logout();
   	}
   });
--- client/templates/includes/register.js
+++ client/templates/includes/register.js
@@ -2,17 +2,21 @@ if (Meteor.isClient) {
   Template.register.events({
     'submit form': function(event){
       event.preventDefault();
+      var usernameVar = event.target.registerUsername.value;
       var emailVar = event.target.registerEmail.value;
       var passwordVar = event.target.registerPassword.value;
-      console.log("Form submitted.");
+      if (usernameVar.length < 1) {
+        return throwError("You must enter a username.");
+      };
       Accounts.createUser({
+        username: usernameVar,
         email: emailVar,
         password: passwordVar
       },
       function(Error) {
         if (Error)
           return throwError(Error);
-        });
+      });
     }
   });
 }
