dbe9bbb6af79b5364c228a3c64d50fb9de4f3a87
Nicholas Gunner <gunnernr@gmail.com>
2016-12-15 12:35:33 -0500

    Basic drag drop enabled

--- client/templates/stories/story_page.js
+++ client/templates/stories/story_page.js
@@ -1,9 +1,3 @@
-Template.storyPage.helpers({
-  tiles: function() {
-    return Tiles.find({storyId: this._id});
-  }
-});
-
 // Set session variable for which tile form to use
 
 Template.storyPage.events({
@@ -13,6 +7,9 @@ Template.storyPage.events({
 });
 
 Template.storyPage.helpers({
+  tiles: function() {
+    return Tiles.find({storyId: this._id}, {sort: {rank: 1}});
+  },
   showTileMenu: function(n) {
     return Session.equals('tileMenu', n);
   },
@@ -20,3 +17,39 @@ Template.storyPage.helpers({
     return this.userId === Meteor.userId();
   }
 });
+
+//Once the Template is rendered, run this function which
+//  sets up JQuery UI's sortable functionality
+Template.storyPage.rendered = function() {
+   this.$('#tile-items').sortable({
+       stop: function(e, ui) {
+         // get the dragged html element and the one before
+         //   and after it
+         el = ui.item.get(0)
+         before = ui.item.prev().get(0)
+         after = ui.item.next().get(0)
+
+         // Here is the part that blew my mind!
+         //  Blaze.getData takes as a parameter an html element
+         //    and will return the data context that was bound when
+         //    that html element was rendered!
+         if(!before) {
+           //if it was dragged into the first position grab the
+           // next element's data context and subtract one from the rank
+           newRank = Blaze.getData(after).rank - 1
+         } else if(!after) {
+           //if it was dragged into the last position grab the
+           //  previous element's data context and add one to the rank
+           newRank = Blaze.getData(before).rank + 1
+         }
+         else
+           //else take the average of the two ranks of the previous
+           // and next elements
+           newRank = (Blaze.getData(after).rank +
+                      Blaze.getData(before).rank)/2
+
+         //update the dragged Item's rank
+         Tiles.update({_id: Blaze.getData(el)._id}, {$set: {rank: newRank}})
+       }
+   })
+ }
