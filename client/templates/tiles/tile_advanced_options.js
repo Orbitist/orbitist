7542a6e8d884ed5d1935304b9c20d0475fdba64d
Nicholas Gunner <gunnernr@gmail.com>
2016-12-23 07:59:16 -0500

    Added drag/drop support to google maps

--- client/templates/tiles/tile_advanced_options.js
+++ client/templates/tiles/tile_advanced_options.js
@@ -1,7 +1,32 @@
 Template.tileAdvancedOptions.onRendered(function() {
   this.autorun(function () {
     if (GoogleMaps.loaded()) {
-      $("#locationSearch").geocomplete({map:'#map', mapOptions:{draggable:false, zoom:1, zoomControl:false, streetViewControl:false, mapTypeControl:false, mapTypeId: 'hybrid', center:{lat: 20, lng: 0}}, details:'.form-group'});
+      $("#locationSearch").geocomplete({
+        map:'#map',
+        mapOptions: {
+          draggable:true,
+          zoom:1,
+          zoomControl:false,
+          streetViewControl:false,
+          mapTypeControl:false,
+          mapTypeId: 'hybrid',
+          center: {
+            lat: 20,
+            lng: 0
+          }
+        },
+        markerOptions: {
+          draggable: true
+        },
+        details: '.form-group'
+      });
+
+      $("#locationSearch").bind("geocode:dragged", function(event, latLng){
+        $("input[name=lat]").val(latLng.lat());
+        $("input[name=lng]").val(latLng.lng());
+        $("#reset").show();
+      });
+
     }
   });
 });
@@ -10,7 +35,35 @@ Template.tileAdvancedOptions.onRendered(function() {
 Template.tileAdvancedOptionsEdit.onRendered(function() {
   this.autorun(function () {
     if (GoogleMaps.loaded()) {
-      $("#locationSearch").geocomplete({map:'#map', mapOptions:{draggable:false, zoom:1, zoomControl:false, streetViewControl:false, mapTypeControl:false, mapTypeId: 'hybrid', center:{lat: 20, lng: 0}}, details:'.form-group'});
+      $("#locationSearch").geocomplete({
+        map:'#map',
+        mapOptions: {
+          draggable:true,
+          zoom:1,
+          zoomControl:false,
+          streetViewControl:false,
+          mapTypeControl:false,
+          mapTypeId: 'hybrid',
+          markerOptions: {
+            draggable: true
+          },
+          center: {
+            lat: 20,
+            lng: 0
+          }
+        },
+        markerOptions: {
+          draggable: true
+        },
+        details: '.form-group'
+      });
+
+      $("#locationSearch").bind("geocode:dragged", function(event, latLng){
+        $("input[name=lat]").val(latLng.lat());
+        $("input[name=lng]").val(latLng.lng());
+        $("#reset").show();
+      });
+
     }
   });
 });
