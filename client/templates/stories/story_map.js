25b71e4fa2515d38191b75e3e64e353a6248ac41
Nicholas Gunner <gunnernr@gmail.com>
2016-12-23 12:24:47 -0500

    close popups on click off

--- client/templates/stories/story_map.js
+++ client/templates/stories/story_map.js
@@ -18,36 +18,41 @@ Template.storyMap.helpers({
 
 Template.storyMap.onRendered(function() {
 
-
   function initMap() {
 
     var bounds = new google.maps.LatLngBounds();
-
     var map = new google.maps.Map(document.getElementById('map'), {
-
+      //Options
     });
 
     Tiles.find({field:this._id}).forEach(function(point) {
-
-      var orbitistIcon = {
-        url: 'https://res.cloudinary.com/orbitist/image/upload/c_scale,w_60/v1481982860/eee0g1qjw3nh4p7doruq.png',
-        size: new google.maps.Size(30, 30),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(15, 30),
-        scaledSize: new google.maps.Size(30, 30)
-      };
-
       if (point.latitude != NaN) {
+        var orbitistIcon = {
+          url: 'https://res.cloudinary.com/orbitist/image/upload/c_scale,w_60/v1481982860/eee0g1qjw3nh4p7doruq.png',
+          size: new google.maps.Size(30, 30),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(15, 30),
+          scaledSize: new google.maps.Size(30, 30)
+        };
+        var infowindow = new google.maps.InfoWindow({
+          content: point.text
+        });
         var marker = new google.maps.Marker({
           position: {lat: point.latitude, lng: point.longitude},
           icon: orbitistIcon,
           map: map
         });
         bounds.extend(marker.position);
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+        google.maps.event.addListener(map, "click", function(event) {
+            infowindow.close();
+        });
       }
     });
-
     map.fitBounds(bounds);
+
   }
 
   initMap();
