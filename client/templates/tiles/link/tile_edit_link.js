9d9fc6f808d740f0edabf7871124b153fc3b9a15
Nicholas Gunner <gunnernr@gmail.com>
2017-01-04 03:41:05 -0500

    Added link template

---
+++ client/templates/tiles/link/tile_edit_link.js
@@ -0,0 +1,59 @@
+Template.tileEditLink.onCreated(function() {
+  Session.set('tileEditLinkErrors', {});
+});
+
+Template.tileEditLink.helpers({
+  errorMessage: function(field) {
+    return Session.get('tileEditLinkErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('tileEditLinkErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.tileEditLink.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentTileId = this._id;
+
+    var $lat = $(e.target).find('[name=lat]');
+    var latInput = Number($lat.val());
+    if (!latInput) {
+      latInput = NaN;
+    }
+
+    var $lng = $(e.target).find('[name=lng]');
+    var lngInput = Number($lng.val());
+    if (!lngInput) {
+      lngInput = NaN;
+    }
+
+    var tileProperties = {
+      link: $(e.target).find('[name=link]').val(),
+      latitude: latInput,
+      longitude: lngInput
+    }
+
+    var errors = validateTile(tileProperties);
+    if (errors.link)
+      return Session.set('tileEditLinkErrors', errors);
+
+    Tiles.update(currentTileId, {$set: tileProperties}, function(error) {
+      if (error) {
+        return throwError(error.reason);
+      }
+    });
+    Router.go('storyEdit', {_id: this.storyId});
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this tile?")) {
+      var currentTileId = this._id;
+      Tiles.remove(currentTileId);
+      Router.go('storyEdit', {_id: this.storyId});
+    }
+  }
+});
---
+++ client/templates/tiles/link/tile_submit_link.js
@@ -0,0 +1,74 @@
+Template.tileSubmitText.onCreated(function() {
+  Session.set('tileSubmitErrors', {});
+});
+
+Template.tileSubmitText.helpers({
+  errorMessage: function(field) {
+    return Session.get('tileSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('tileSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.tileSubmitText.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+
+    var topTile = Tiles.findOne({storyId: template.data._id}, {sort: {rank: 1}});
+    if (topTile) {
+      var topRank = topTile.rank - 1;
+    } else {
+      var topRank = 1;
+    }
+
+    var $lat = $(e.target).find('[name=lat]');
+    var latInput = Number($lat.val());
+    if (!latInput) {
+      latInput = NaN;
+    }
+
+    var $lng = $(e.target).find('[name=lng]');
+    var lngInput = Number($lng.val());
+    if (!lngInput) {
+      lngInput = NaN;
+    }
+
+    var $text = $(e.target).find('[name=text]');
+    var tile = {
+      tileType: 'text',
+      text: $text.val(),
+      title: '',
+      storyId: template.data._id,
+      imageUrl: '',
+      imageId: '',
+      videoUrl: '',
+      videoId: '',
+      embed: '',
+      phone: '',
+      email: '',
+      hours: '',
+      cost: '',
+      accessibility: '',
+      rank: topRank,
+      latitude: latInput,
+      longitude: lngInput
+    };
+
+    var errors = {};
+    if (! tile.text) {
+      errors.text = "Please write some content";
+      return Session.set('tileSubmitErrors', errors);
+    }
+
+    Meteor.call('tileInsert', tile, function(error, tileId) {
+      if (error){
+        throwError(error.reason);
+      } else {
+        $text.val('');
+        Session.set('tileMenu', 'false');
+      }
+    });
+  }
+
+});
