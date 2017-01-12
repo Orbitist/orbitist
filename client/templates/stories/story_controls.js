ef6ffefbd4f4833d253dd40895d4276b863e6715
Nicholas Gunner <gunnernr@gmail.com>
2017-01-12 08:20:33 -0500

    Working on the map functions

--- client/templates/stories/story_controls.js
+++ client/templates/stories/story_controls.js
@@ -2,14 +2,8 @@ Template.storyControls.helpers({
   ownStory: function() {
     return this.userId === Meteor.userId();
   },
-  isStoryEditPage: function() {
-    return Router.current().route.getName() == 'storyEdit'
-  },
-  isStoryMapPage: function() {
-    return Router.current().route.getName() == 'storyMap'
-  },
-  isStoryPage: function() {
-    return Router.current().route.getName() == 'storyPage'
+  isStoryMap: function(n) {
+    return Session.equals('storyMap', n);
   }
 });
 
